6816067851fbc811842f6140bc835f2f881e9830
Vincent Pantia <vincent.m.pantia@hotmail.com>
2023-03-21 15:12:54 +0800

    [+] Add MoneyMe.Web

---
+++ Code/MoneyMe.Web/MoneyMe.Web/Models/QuotationViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace MoneyMe.Web.Models
+{
+    public class QuotationViewModel
+    {
+        public decimal AmountRequired { get; set; }
+        public decimal Term { get; set; }
+        public string Title { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+    }
+}
